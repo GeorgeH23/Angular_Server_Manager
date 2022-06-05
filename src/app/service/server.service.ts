9cebb4cc9503ceceec537df4a873449d86324b53
GeorgeH23 <gheorghita.harpa0423@yahoo.com>
2022-06-05 20:49:00 +0100

    Server service part 2

--- src/app/service/server.service.ts
+++ src/app/service/server.service.ts
@@ -1,8 +1,10 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, tap} from 'rxjs/operators';
+import { Status } from '../enum/status.enum';
 import { CustomResponse } from '../interface/custom-response';
+import { Server } from '../interface/server';
 
 @Injectable({providedIn: 'root'})
 export class ServerService {
@@ -22,7 +24,29 @@ export class ServerService {
     catchError(this.handleError)
   );
 
-  handleError(handleError: any): Observable<never> {
-    return throwError ('Method not implemented.');
+  save$ = (server: Server) => <Observable<CustomResponse>>
+  this.http.post<CustomResponse>(`${this.apiUrl}/server/save`, server)
+  .pipe(
+    tap(console.log),
+    catchError(this.handleError)
+  );
+
+  ping$ = (ipAddress: string) => <Observable<CustomResponse>>
+  this.http.get<CustomResponse>(`${this.apiUrl}/server/ping/${ipAddress}`)
+  .pipe(
+    tap(console.log),
+    catchError(this.handleError)
+  );
+
+  delete$ = (serverId: number) => <Observable<CustomResponse>>
+  this.http.delete<CustomResponse>(`${this.apiUrl}/server/delete/${serverId}`)
+  .pipe(
+    tap(console.log),
+    catchError(this.handleError)
+  );
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    console.log(error);
+    return throwError (`An error occured - Error code: ${error.status}`);
   }            
 }
